9029f3d34b6d1114809ae567a283963c01516c67
fernando481 <115594712+fernando481@users.noreply.github.com>
2025-01-28 18:48:18 +0000

    3 commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,34 @@
+public class BankAccount {
+
+
+
+    public static void main(String[] args) {
+       double   total = 2175.37;
+        System.out.println("Total money: 2175.37");
+        total = total - 302.50;
+        total = total + 50.03;
+        total = total /2;
+        total = total + 20.00;
+        total = total - 1.00;
+        total = total * 2 ;
+        total = total + 1.00;
+
+        System.out.println( "The new total is :"  + total );
+
+
+
+
+
+
+
+
+
+	}
+
+
+    
+} 
+
+
+
 
